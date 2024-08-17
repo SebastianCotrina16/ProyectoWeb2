90c3ebc443388007a03be2badefcf75db4cec980
Sebastian Cotrina <102818449+SebastianCotrina16@users.noreply.github.com>
2024-08-17 09:45:35 -0500

    Update app.py

--- shot-evaluation/app.py
+++ shot-evaluation/app.py
@@ -1,4 +1,10 @@
+"""
+A Flask application for detecting objects in images using a pre-trained Faster R-CNN model.
+Handles file uploads, performs object detection, and records results in a SQL database.
+"""
+
 import os
+from datetime import datetime
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 from PIL import Image
@@ -7,13 +13,12 @@ import torchvision.transforms as T
 from torchvision.models.detection import fasterrcnn_resnet50_fpn
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 import pyodbc
-from datetime import datetime
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024     
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-conn_str = (
+CONNECTION_STRING= (
     'DRIVER={ODBC Driver 17 for SQL Server};'
     'SERVER=svrweb2frevb.database.windows.net;'
     'DATABASE=sistemabalas;'
@@ -25,37 +30,99 @@ conn_str = (
 )
 
 def get_db_connection():
-    return pyodbc.connect(conn_str)
+    """
+    Establishes and returns a connection to the database.
+    """
+    return pyodbc.connect(CONNECTION_STRING) # pylint: disable=E1101
 
 def get_model(num_classes):
-    model = fasterrcnn_resnet50_fpn(weights='FasterRCNN_ResNet50_FPN_Weights.COCO_V1')
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-    return model
+    """
+    Loads and prepares the Faster R-CNN model for object detection.
+    Args:
+        num_classes (int): The number of classes the model should recognize.
+    Returns:
+        torch.nn.Module: The prepared Faster R-CNN model.
+    """
+    model_loaded = fasterrcnn_resnet50_fpn(weights='FasterRCNN_ResNet50_FPN_Weights.COCO_V1')
+    in_features = model_loaded.roi_heads.box_predictor.cls_score.in_features
+    model_loaded.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    return model_loaded
 
-num_classes = 2
-model = get_model(num_classes)
-model.load_state_dict(torch.load('model/bullet_detection_model.pth', map_location=torch.device('cpu')))
+NUM_CLASSES = 2
+model = get_model(NUM_CLASSES)
+model.load_state_dict(torch.load(
+    'model/bullet_detection_model.pth',
+    map_location=torch.device('cpu')
+    ))
 model.eval()
 
 transform = T.Compose([
     T.ToTensor()
 ])
 
+def process_image(file):
+    """Process the uploaded image and return the image tensor."""
+    filename = secure_filename(file.filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+    image = Image.open(filepath).resize((800, 1000))
+    return transform(image).unsqueeze(0), filepath
+
+def insert_impact_data(id_usuario, filepath, boxes, scores):
+    """Insert impact data into the database."""
+    total_precision = 0
+    disparos = []
+    for box, score in zip(boxes, scores):
+        x1, y1, x2, y2 = box
+        ubicacion = f'({(x1 + x2) / 2}, {(y1 + y2) / 2})'
+        precision = float(score)
+        total_precision += precision
+        disparos.append({
+            'ubicacion': ubicacion,
+            'precision': precision
+        })
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                "INSERT INTO ImpactosBala (IdUsuario, Fecha, Ubicacion, Precision, RutaImagen) "
+                "VALUES (?, ?, ?, ?, ?)",
+                id_usuario, datetime.now(), ubicacion, precision, filepath
+            )
+            conn.commit()
+    return disparos, total_precision
+
+def insert_report_data(id_usuario, total_impactos, promedio_precision):
+    """Insert report data into the database."""
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO Reportes(IdUsuario,FechaReporte,TotalImpactos,PromedioPrecision,Detalles) "
+            "VALUES (?, ?, ?, ?, ?)",
+            id_usuario,
+            datetime.now(),
+            total_impactos,
+            promedio_precision,
+            'Detalles del reporte de impacto de balas.'
+        )
+        conn.commit()
+
 @app.route('/detect', methods=['POST'])
 def detect():
+    """
+        Endpoint para detectar objetos en una imagen enviada.
+
+        Recibe una imagen y un identificador de usuario a través de una solicitud POST. 
+        Guarda la imagen, realiza la detección con un modelo Faster R-CNN, 
+        y almacena los resultados en la base de datos SQL. 
+        Retorna un JSON con las ubicaciones y precisiones de los impactos detectados.
+    """
     file = request.files.get('imageName')
     id_usuario = request.form.get('idUsuario')
 
     if not file:
         return jsonify({'status': 'error', 'message': 'El campo de imagen es obligatorio'}), 400
 
-    filename = secure_filename(file.filename)
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(filepath)
-
-    image = Image.open(filepath).resize((800, 1000))
-    image_tensor = transform(image).unsqueeze(0)
+    image_tensor, filepath = process_image(file)
 
     with torch.no_grad():
         predictions = model(image_tensor)
@@ -67,38 +134,14 @@ def detect():
     boxes = prediction_data[scores > threshold]
     scores = scores[scores > threshold]
 
-    disparos = []
-    total_precision = 0
-    for box, score in zip(boxes, scores):
-        x1, y1, x2, y2 = box
-        ubicacion = f'({(x1 + x2) / 2}, {(y1 + y2) / 2})'
-        precision = float(score)
-        total_precision += precision
-        disparos.append({
-            'ubicacion': ubicacion,
-            'precision': precision
-        })
-
-        with get_db_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                "INSERT INTO ImpactosBala (IdUsuario, Fecha, Ubicacion, Precision, RutaImagen) VALUES (?, ?, ?, ?, ?)",
-                id_usuario, datetime.now(), ubicacion, precision, filepath
-            )
-            conn.commit()
-
+    disparos, total_precision = insert_impact_data(id_usuario, filepath, boxes, scores)
     promedio_precision = total_precision / len(disparos) if disparos else 0
-
-    with get_db_connection() as conn:
-        cursor = conn.cursor()
-        cursor.execute(
-            "INSERT INTO Reportes (IdUsuario, FechaReporte, TotalImpactos, PromedioPrecision, Detalles) VALUES (?, ?, ?, ?, ?)",
-            id_usuario, datetime.now(), len(disparos), promedio_precision, 'Detalles del reporte de impacto de balas.'
-        )
-        conn.commit()
+    insert_report_data(id_usuario, len(disparos), promedio_precision)
 
     return jsonify({'disparos': disparos})
+
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     app.run(debug=True, host='0.0.0.0', port=5008)
+
