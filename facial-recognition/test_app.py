70a17a076b855305fdd7f5eb177325d0dec320f5
Sebastian Cotrina <102818449+SebastianCotrina16@users.noreply.github.com>
2024-08-17 00:40:19 -0500

    Update test_app.py

--- facial-recognition/test_app.py
+++ facial-recognition/test_app.py
@@ -1,24 +1,31 @@
+"""
+This module contains the unit tests for the Flask application.
+"""
 import unittest
-from io import BytesIO
 from flask import json
-from app import app, SessionLocal, User, Base, engine
+from app import app, SessionLocal, User, Base, engine  # pylint: disable=E0401
+
 
 class FaceRecognitionTestCase(unittest.TestCase):
+    """
+    Unit tests suite for the Flask application.
+    """
 
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
 
-        # Create a new database session for each test
         Base.metadata.create_all(bind=engine)
         self.session = SessionLocal()
 
     def tearDown(self):
-        # Close the session and drop all tables after each test
         self.session.close()
         Base.metadata.drop_all(bind=engine)
 
     def test_register_user(self):
+        """
+        Test that a user can be registered in the database.
+        """
         with open('Fotos/SebastianCotrina.png', 'rb') as img:
             response = self.app.post('/register', data={
                 'dni': '12345678',
@@ -29,13 +36,14 @@ class FaceRecognitionTestCase(unittest.TestCase):
         data = json.loads(response.data)
         self.assertTrue(data['success'])
 
-        # Check if user is added to the database
         user = self.session.query(User).filter_by(dni='12345678').first()
         self.assertIsNotNone(user)
         self.assertEqual(user.name, 'John Doe')
 
     def test_recognize_user(self):
-        # First, register a user
+        """
+        Test that a registered user can be recognized.
+        """
         with open('Fotos/Keanu.webp', 'rb') as img:
             self.app.post('/register', data={
                 'dni': '12345678',
@@ -43,7 +51,6 @@ class FaceRecognitionTestCase(unittest.TestCase):
                 'image': img
             })
 
-        # Then, try to recognize the same user
         with open('Fotos/Keanu2.webp', 'rb') as img:
             response = self.app.post('/recognize', data={'image': img})
 
@@ -53,11 +60,16 @@ class FaceRecognitionTestCase(unittest.TestCase):
         self.assertEqual(data['user']['name'], 'John Doe')
 
     def test_recognize_unknown_user(self):
+        """
+        Test that an unknown user is not recognized.
+        """
         with open('Fotos/Adam.jpg', 'rb') as img:
             response = self.app.post('/recognize', data={'image': img})
 
         data = json.loads(response.data)
         self.assertIsNone(data['user'])
 
+
 if __name__ == '__main__':
     unittest.main()
+
